8ab3a6a6db7eaf7daa1e76031682cdd8d733d512
Cleysso Schwanck <cleysso.s@edu.pucrs.br>
2025-05-09 08:18:35 -0300

    Alteracoes da aula do dia 07_05

--- bullsCows.sv
+++ bullsCows.sv
@@ -1,5 +1,6 @@
 module BullsCows (
-    input [16:0] guess,
+    input [15:0] guess,
+    input confirm,
     input clock,
     input reset
 );
@@ -14,8 +15,10 @@ module BullsCows (
 
     state_t state, next_state;
     logic [2:0] bulls, cows;
-    logic [16:0] secret_j1, secret_j2;
-    logic [16:0] guess_j1, guess_j2;
+    logic [15:0] secret_j1, secret_j2;
+    logic [15:0] guess_i;
+    logic confirmed;
+
 
 
     always_ff @(posedge clock or posedge reset) begin
@@ -25,19 +28,29 @@ module BullsCows (
             state <= next_state;
             case (state)
                 SECRET_J1: begin
-                    secret_j1 <= guess;
+                    if (confirmed) begin
+                        if (guess_i == not~(guess_i[15:12] ^ guess_i[11:8] ^ guess_i[7:4] ^ guess_i[3:0])) begin
+                            secret_j1 <= guess_i;
+                            next_state <= SECRET_J2;
+                    end
+                    confirmed <= 0;
                 end
                 SECRET_J2: begin
-                    secret_j2 <= guess;
+                    secret_j2 <= guess_i;
                 end
                 GUESS_J1: begin
-                    guess_j1 <= guess;
+                    guess_j1 <= guess_i;
                 end
                 GUESS_J2: begin
-                    guess_j2 <= guess;
+                    guess_j2 <= guess_i;
                 end
+            endcase
         end
     end
 
+    always_ff @(posedge confirm) begin
+        guess_i <= guess;
+        confirmed <= 1;
+    end
 
 endmodule
